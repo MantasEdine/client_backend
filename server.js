8c014a07c56f7f3706caa3f40710e38f4c67210d
MantasEdine <youcefmantas3@gmail.com>
2025-10-20 16:27:10 -0700

    ff

--- server.js
+++ server.js
@@ -25,10 +25,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
+
 
 // Middleware
 app.use(cors());
