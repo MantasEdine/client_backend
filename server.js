b88eddcf59c55132a3ad681bef8f47f8fac6e8e7
MantasEdine <youcefmantas3@gmail.com>
2025-10-20 17:48:08 -0700

    ff

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ const io = new Server(server, {
 
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
